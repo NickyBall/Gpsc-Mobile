9b2085b625acd97099365df0616717204f902370
NickyBall <ball.vong.yo.gh@gmail.com>
2017-12-24 21:18:27 +0700

    world map add marker with mockup data and on click listener

--- src/pages/world/world.ts
+++ src/pages/world/world.ts
@@ -1,5 +1,14 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {
+ GoogleMaps,
+ GoogleMap,
+ GoogleMapsEvent,
+ GoogleMapOptions,
+ CameraPosition,
+ MarkerOptions,
+ Marker
+} from '@ionic-native/google-maps';
 
 /**
  * Generated class for the WorldPage page.
@@ -14,12 +23,256 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'world.html',
 })
 export class WorldPage {
+  map: GoogleMap;
+  countryList = JSON.parse(`[
+  {
+    "order": 1,
+    "name": "THAILAND",
+    "geolocation": {
+      "lat": 15.87,
+      "lng": 100.9925
+    },
+    "categories": [
+      {
+        "name": "powerPlant",
+        "imgUrl": "http://via.placeholder.com/50x50"
+      },
+      {
+        "name": "solarPlant",
+        "imgUrl": "http://via.placeholder.com/50x50"
+      }
+    ]
+  },
+  {
+    "order": 2,
+    "name": "LAOS",
+    "geolocation": {
+      "lat": 19.8563,
+      "lng": 102.4955
+    },
+    "categories": [
+      {
+        "name": "damPlant",
+        "imgUrl": "http://via.placeholder.com/50x50"
+      }
+    ]
+  },
+  {
+    "order": 3,
+    "name": "JAPAN",
+    "geolocation": {
+      "lat": 36.2048,
+      "lng": 138.2529
+    },
+    "categories": [
+      {
+        "name": "solarPlant",
+        "imgUrl": "http://via.placeholder.com/50x50"
+      }
+    ]
+  },
+  {
+    "order": 4,
+    "name": "USA",
+    "geolocation": {
+      "lat": 42.877742,
+      "lng": -97.380979
+    },
+    "categories": [
+      {
+        "name": "windPlant",
+        "imgUrl": "http://via.placeholder.com/50x50"
+      }
+    ]
+  },
+  {
+    "order": 5,
+    "name": "MYANMAR",
+    "geolocation": {
+      "lat": 21.9162,
+      "lng": 95.956
+    },
+    "categories": [
+      {
+        "name": "powerPlant",
+        "imgUrl": "http://via.placeholder.com/50x50"
+      },
+      {
+        "name": "damPlant",
+        "imgUrl": "http://via.placeholder.com/50x50"
+      }
+    ]
+  }
+]`);
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private googleMap: GoogleMaps) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad WorldPage');
+    this.loadMap();
+  }
+
+  loadMap(){
+    let mapOptions: GoogleMapOptions = {
+      camera: {
+        target: {
+          lat: 15.87,
+          lng: 100.9925
+        },
+        zoom: 1,
+        tilt: 0
+      },
+      styles: [
+        {
+          "featureType":"all",
+          "elementType":"all",
+          "stylers":[
+            {
+              "hue":"#e7ecf0"
+            }
+          ]
+        },
+        {
+          "featureType":"administrative.province",
+          "elementType":"all",
+          "stylers":[
+            {
+              "visibility":"off"
+            }
+          ]
+        },
+        {
+          "featureType":"landscape",
+          "elementType":"all",
+          "stylers":[
+            {
+              "gamma":"3.62"
+            },
+            {
+              "lightness":"-2"
+            },
+            {
+              "saturation":"-46"
+            },
+            {
+              "weight":"1.39"
+            },
+            {
+              "visibility":"on"
+            },
+            {
+              "color":"#ededed"
+            }
+          ]
+        },
+        {
+          "featureType":"poi",
+          "elementType":"all",
+          "stylers":[
+            {
+              "visibility":"off"
+            }
+          ]
+        },
+        {
+          "featureType":"road",
+          "elementType":"all",
+          "stylers":[
+            {
+              "saturation":"-61"
+            },
+            {
+              "visibility":"on"
+            },
+            {
+              "lightness":"20"
+            },
+            {
+              "gamma":"1.10"
+            },
+            {
+              "weight":"1.99"
+            }
+          ]
+        },
+        {
+          "featureType":"road",
+          "elementType":"geometry",
+          "stylers":[
+            {
+              "weight":"0.99"
+            }
+          ]
+        },
+        {
+          "featureType":"road",
+          "elementType":"labels",
+          "stylers":[
+            {
+              "visibility":"off"
+            }
+          ]
+        },
+        {
+          "featureType":"transit",
+          "elementType":"all",
+          "stylers":[
+            {
+              "visibility":"off"
+            }
+          ]
+        },
+        {
+          "featureType":"water",
+          "elementType":"all",
+          "stylers":[
+            {
+              "visibility":"simplified"
+            },
+            {
+              "saturation":"-45"
+            },
+            {
+              "gamma":"1"
+            },
+            {
+              "lightness":"-4"
+            }
+          ]
+        }
+      ]
+    };
+
+    this.map = this.googleMap.create('map_canvas', mapOptions);
+
+    // Wait the MAP_READY before using any methods.
+    this.map.one(GoogleMapsEvent.MAP_READY)
+      .then(() => {
+        console.log('Map is ready!');
+        this.countryList.forEach((country) => {
+          this.map.addMarker({
+              title: country.name,
+              icon: 'blue',
+              animation: 'DROP',
+              position: {
+                lat: country.geolocation.lat,
+                lng: country.geolocation.lng
+              }
+            })
+            .then(marker => {
+              marker.on(GoogleMapsEvent.MARKER_CLICK)
+                .subscribe(() => {
+                  // alert('clicked');
+                });
+            });
+        });
+
+      });
+  }
+
+  selectedCountry(item) {
+    alert(item.name);
   }
 
 }
