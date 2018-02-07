b59e9168d22ceb185a6b09bcc1de3c7707690205
Jakkrit Junrat <j.jakkrit@appswin.co.th>
2018-02-07 21:58:19 +0700

    Fix Gauge

--- src/pages/summary/summary.ts
+++ src/pages/summary/summary.ts
@@ -24,81 +24,84 @@ export class SummaryPage {
   }
 
   ionViewDidLoad() {
-       
+    this.powerGenGraph();
+  }
 
+  powerGenGraph() {
+    this.gaugeChart = HighCharts.chart('gauge-chart', {
+        
+        chart: {
+            type: 'gauge',
+            plotBackgroundColor: null,
+            plotBackgroundImage: null,
+            plotBorderWidth: 0,
+            plotShadow: false
+        },
+    
+        title: {
+            text: 'Speedometer'
+        },
+    
+        pane: {
+            startAngle: -100,
+            endAngle: 100,
+            background: {
+                backgroundColor: '#efefef',
+                borderWidth: 0,
+                outerRadius: '104%',
+                innerRadius: '107%',
+                shape: 'squre'
+            }
+        },
+    
+        // the value axis
+        yAxis: {
+          min: 0,
+          max: 5,
+          lineColor: '#efefef',
+          tickColor: '#efefef',
+          minorTickColor: 'transparent',
+          tickPixelInterval: 1,
+          tickInterval: 1,
+          lineWidth: 2,
+          labels: {
+              distance: -20,
+              rotation: 0
+          },
+          tickLength: 15,
+          minorTickLength: 5,
+          endOnTick: false,
+            plotBands: [{
+                from: 0,
+                to: 100,
+                color:  {
+                  linearGradient: { x1: 0, y1: 0.5, x2: 1, y2: 0.5 },
+                  stops: [
+                      [0, '#19f3ff'],
+                      [1, '#1847ff']
+                  ]
+              }
+            }]
+        },
+    
+        series: [{
+            name: 'Speed',
+            data: [3.5],
+            dataLabels: false,
+            tooltip: {
+                valueSuffix: ' km/h'
+            }
+        }],
+
+        credits: {
+          enabled: false
+      },
+    
+    });
   }
 
   segmentClick(selectedSection) {
-    this.gaugeChart = HighCharts.chart('gauge-chart', {
-        
-                chart: {
-                    type: 'gauge',
-                    plotBackgroundColor: null,
-                    plotBackgroundImage: null,
-                    plotBorderWidth: 0,
-                    plotShadow: false
-                },
-            
-                title: {
-                    text: 'Speedometer'
-                },
-            
-                pane: {
-                    startAngle: -100,
-                    endAngle: 100,
-                    background: {
-                        backgroundColor: '#efefef',
-                        borderWidth: 0,
-                        outerRadius: '104%',
-                        innerRadius: '107%',
-                        shape: 'squre'
-                    }
-                },
-            
-                // the value axis
-                yAxis: {
-                  min: 0,
-                  max: 5,
-                  lineColor: '#efefef',
-                  tickColor: '#efefef',
-                  minorTickColor: 'transparent',
-                  tickPixelInterval: 1,
-                  tickInterval: 1,
-                  lineWidth: 2,
-                  labels: {
-                      distance: -20,
-                      rotation: 0
-                  },
-                  tickLength: 15,
-                  minorTickLength: 5,
-                  endOnTick: false,
-                    plotBands: [{
-                        from: 0,
-                        to: 100,
-                        color:  {
-                          linearGradient: { x1: 0, y1: 0.5, x2: 1, y2: 0.5 },
-                          stops: [
-                              [0, '#19f3ff'],
-                              [1, '#1847ff']
-                          ]
-                      }
-                    }]
-                },
-            
-                series: [{
-                    name: 'Speed',
-                    data: [3.5],
-                    dataLabels: false,
-                    tooltip: {
-                        valueSuffix: ' km/h'
-                    }
-                }],
-        
-                credits: {
-                  enabled: false
-              },
-            
-            })
+    
   }
 
 }
