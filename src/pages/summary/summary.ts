c904212736cb2a90f555dd8377f15319acba4cf8
DESKTOP-9N4VL75\TTNG <praiwt.coe@gmail.com>
2018-02-07 21:56:39 +0700

    enhance theme

--- src/pages/summary/summary.ts
+++ src/pages/summary/summary.ts
@@ -20,85 +20,93 @@ export class SummaryPage {
   selectedSection = 'powerGenerationTab';
   gaugeChart: any;
   hc: any;
+  powerIcon: any;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
-       
+    this.powerIcon = "assets/imgs/b3.png";  // temp
 
   }
 
   segmentClick(selectedSection) {
-    this.gaugeChart = HighCharts.chart('gauge-chart', {
-        
-                chart: {
-                    type: 'gauge',
-                    plotBackgroundColor: null,
-                    plotBackgroundImage: null,
-                    plotBorderWidth: 0,
-                    plotShadow: false
-                },
-            
-                title: {
-                    text: 'Speedometer'
-                },
-            
-                pane: {
-                    startAngle: -100,
-                    endAngle: 100,
-                    background: {
-                        backgroundColor: '#efefef',
-                        borderWidth: 0,
-                        outerRadius: '104%',
-                        innerRadius: '107%',
-                        shape: 'squre'
-                    }
-                },
-            
-                // the value axis
-                yAxis: {
-                  min: 0,
-                  max: 5,
-                  lineColor: '#efefef',
-                  tickColor: '#efefef',
-                  minorTickColor: 'transparent',
-                  tickPixelInterval: 1,
-                  tickInterval: 1,
-                  lineWidth: 2,
-                  labels: {
-                      distance: -20,
-                      rotation: 0
-                  },
-                  tickLength: 15,
-                  minorTickLength: 5,
-                  endOnTick: false,
-                    plotBands: [{
-                        from: 0,
-                        to: 100,
-                        color:  {
-                          linearGradient: { x1: 0, y1: 0.5, x2: 1, y2: 0.5 },
-                          stops: [
-                              [0, '#19f3ff'],
-                              [1, '#1847ff']
-                          ]
-                      }
-                    }]
-                },
-            
-                series: [{
-                    name: 'Speed',
-                    data: [3.5],
-                    dataLabels: false,
-                    tooltip: {
-                        valueSuffix: ' km/h'
-                    }
-                }],
-        
-                credits: {
-                  enabled: false
-              },
-            
-            })
+    this.gaugeChart = HighCharts.chart('power-chart', {
+        chart: {
+            type: 'gauge',
+            plotBackgroundColor: null,
+            plotBackgroundImage: null,
+            plotBorderWidth: 0,
+            height: '110%',
+            plotShadow: false
+        },
+    
+        title: {
+            text: ''
+        },
+    
+        pane: {
+            startAngle: -100,
+            endAngle: 100,
+            background: {
+                backgroundColor: '#efefef',
+                borderWidth: 0,
+                outerRadius: '104%',
+                innerRadius: '107%',
+                shape: 'squre'
+            }
+        },
+    
+        // the value axis
+        yAxis: {
+            min: 0,
+            max: 5,
+            lineColor: '#efefef',
+            tickColor: '#efefef',
+            minorTickColor: 'transparent',
+            tickPixelInterval: 1,
+            tickInterval: 1,
+            lineWidth: 2,
+            labels: {
+                distance: -20,
+                rotation: 0
+            },
+            tickLength: 15,
+            minorTickLength: 5,
+            endOnTick: false,
+            plotBands: [{
+                from: 0,
+                to: 100,
+                color:  {
+                    linearGradient: { x1: 0, y1: 0.5, x2: 1, y2: 0.5 },
+                    stops: [
+                        [0, '#19f3ff'],
+                        [1, '#1847ff']
+                    ]
+                }
+            }]
+        },
+    
+        series: [{
+            name: 'Power',
+            data: [3.5],
+            dataLabels: false,
+            tooltip: {
+                valueSuffix: ' MW'
+            }
+        }],
+
+        dataLabels: {
+            enabled: true,
+            useHTML: true,
+            formatter: function() {
+                return '<p>OK</p>';
+            }
+        },
+        credits: {
+            enabled: false
+        },
+    
+    })
   }
 
 }
