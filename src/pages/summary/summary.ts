89faf69a1dde325b78cf1e6db3f9bf7c9d6fccae
DESKTOP-9N4VL75\TTNG <praiwt.coe@gmail.com>
2018-02-09 00:49:52 +0700

    add icon and energy tab

--- src/pages/summary/summary.ts
+++ src/pages/summary/summary.ts
@@ -1,5 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Chart } from 'chart.js';
 import * as HighCharts from 'highcharts';
 import * as HighchartsMore from 'highcharts/highcharts-more';
 HighchartsMore(HighCharts);
@@ -17,6 +18,12 @@ HighchartsMore(HighCharts);
   templateUrl: 'summary.html',
 })
 export class SummaryPage {
+
+  @ViewChild('hourlyCanvas') hourlyCanvas;
+  @ViewChild('dailyCanvas') dailyCanvas;
+  @ViewChild('monthlyCanvas') monthlyCanvas;
+  @ViewChild('yearlyCanvas') yearlyCanvas;
+
   selectedSection = 'powerGenerationTab';
 
   powerChart: any;
@@ -31,18 +38,34 @@ export class SummaryPage {
   ambientTempData: any;
   ambientIcon: string;
 
-  enegyChart: any;
-  enegyData: any;
+  hourly: any;
+  hourlyData: any;
+  dialy: any;
+  dialyData: any;
+  monthly: any;
+  monthlyData: any;
+  yearly: any;
+  yearlyData: any;
+
+
   generationSummaryChart: any;
   generationSummaryData: any;
+
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
 
   ionViewDidEnter() {
-    this.powerIcon = "assets/imgs/b3.png";
+    this.powerIcon = "assets/imgs/Elect.png";
+    this.irradiationIcon = "assets/imgs/Sun.png";
+    this.ambientIcon = "assets/imgs/Temp.png";
+
     this.powerGenGraph();
+
+    this.powerData = 3.5;
+    this.irradiationData = 700;
+    this.ambientTempData = 30;
     // console.log(this.irradChart);
     // this.irradiationGraph();
     // this.ambientTemperatureGraph();
@@ -60,6 +83,7 @@ export class SummaryPage {
                 plotBackgroundImage: null,
                 plotBorderWidth: 0,
                 height: '100%',
+                margin: [0, 0, 0, 0],
                 plotShadow: false
             },
         
@@ -111,20 +135,13 @@ export class SummaryPage {
         
             series: [{
                 name: 'Power',
-                data: [3.5],
+                data: [this.powerData],
                 dataLabels: false,
                 tooltip: {
                     valueSuffix: ' MW'
                 }
             }],
-    
-            dataLabels: {
-                enabled: true,
-                useHTML: true,
-                formatter: function() {
-                    return '<p>OK</p>';
-                }
-            },
+
             credits: {
                 enabled: false
             },
@@ -144,6 +161,7 @@ export class SummaryPage {
                 plotBackgroundImage: null,
                 plotBorderWidth: 0,
                 height: '100%',
+                margin: [0, 0, 0, 0],
                 plotShadow: false
             },
         
@@ -174,7 +192,7 @@ export class SummaryPage {
               tickInterval: 200,
               lineWidth: 2,
               labels: {
-                  distance: -20,
+                  distance: -25,
                   rotation: 0
               },
               tickLength: 15,
@@ -194,21 +212,14 @@ export class SummaryPage {
             },
         
             series: [{
-                name: 'Power',
-                data: [700],
+                name: 'Irradiation',
+                data: [this.irradiationData],
                 dataLabels: false,
                 tooltip: {
-                    valueSuffix: ' MW'
+                    valueSuffix: ' W/M<sup>2</sup>'
                 }
             }],
-    
-            dataLabels: {
-                enabled: true,
-                useHTML: true,
-                formatter: function() {
-                    return '<p>OK</p>';
-                }
-            },
+ 
             credits: {
                 enabled: false
             },
@@ -228,7 +239,8 @@ export class SummaryPage {
                 plotBackgroundColor: null,
                 plotBackgroundImage: null,
                 plotBorderWidth: 0,
-                height: '110%',
+                height: '100%',
+                margin: [0, 0, 0, 0],
                 plotShadow: false
             },
         
@@ -250,13 +262,13 @@ export class SummaryPage {
         
             // the value axis
             yAxis: {
-              min: 0,
-              max: 10,
+              min: -40,
+              max: 60,
               lineColor: '#efefef',
               tickColor: '#efefef',
               minorTickColor: 'transparent',
-              tickPixelInterval: 1,
-              tickInterval: 1,
+              tickPixelInterval: 20,
+              tickInterval: 20,
               lineWidth: 2,
               labels: {
                   distance: -20,
@@ -266,8 +278,8 @@ export class SummaryPage {
               minorTickLength: 5,
               endOnTick: false,
                 plotBands: [{
-                    from: 0,
-                    to: 100,
+                    from: -40,
+                    to: 60,
                     color:  {
                       linearGradient: { x1: 0, y1: 0.5, x2: 1, y2: 0.5 },
                       stops: [
@@ -279,11 +291,11 @@ export class SummaryPage {
             },
         
             series: [{
-                name: 'Power',
-                data: [3.5],
+                name: 'Ambient',
+                data: [this.ambientTempData],
                 dataLabels: false,
                 tooltip: {
-                    valueSuffix: ' MW'
+                    valueSuffix: ' <sup>o</sup>C'
                 }
             }],
             credits: {
@@ -297,6 +309,137 @@ export class SummaryPage {
   }
 
   enegyGenerationGraph() {
+    setTimeout(() => {
+        this.hourlyGraph();
+    }, 500);
+  }
+
+  hourlyGraph(){
+    setTimeout(() => {
+        this.hourly = new Chart(this.hourlyCanvas.nativeElement, {     
+            type: 'bar',
+            data: {
+              labels: ["7am", "8am", "9am", "10am", "11am", "12am", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm"],
+              datasets: [
+                  {
+                  lineTension: 0.1,
+                  backgroundColor: "rgba(22, 189, 231, 0.918)",
+                  data: [22, 32, 34, 16, 32, 40, 35, 34, 29, 30, 31, 21],
+                  }
+                ]
+              },
+              options: {
+                legend: {
+                  display: false
+                },
+                tooltips: {
+                  enabled: false
+                },
+                scales: {
+                  xAxes: [{
+                    barPercentage: 0.5,
+                  }]
+                  }
+             }
+     
+        });
+    }, 100);
+  }
+
+  dailyGraph(){
+    setTimeout(() => {
+        this.dialy = new Chart(this.dailyCanvas.nativeElement, {     
+            type: 'bar',
+            data: {
+              labels: ["7Jan", "8Jan", "9Jan", "10Jan", "11Jan", "12Jan", "1Jan"],
+              datasets: [
+                  {
+                  lineTension: 0.1,
+                  backgroundColor: "rgba(22, 189, 231, 0.918)",
+                  data: [22, 32, 34, 16, 32, 40, 35],
+                  }
+                ]
+              },
+              options: {
+                legend: {
+                  display: false
+                },
+                tooltips: {
+                  enabled: false
+                },
+                scales: {
+                  xAxes: [{
+                    barPercentage: 0.5,
+                  }]
+                  }
+             }
+
+        });
+    }, 100);
+  }
+
+  monthlyGraph(){
+    setTimeout(() => {
+        this.monthly = new Chart(this.monthlyCanvas.nativeElement, {     
+            type: 'bar',
+            data: {
+              labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+              datasets: [
+                  {
+                  lineTension: 0.1,
+                  backgroundColor: "rgba(22, 189, 231, 0.918)",
+                  data: [22, 14, 34, 16, 32, 40, 33, 20, 29, 30, 35, 21],
+                  }
+                ]
+              },
+              options: {
+                legend: {
+                  display: false
+                },
+                tooltips: {
+                  enabled: false
+                },
+                scales: {
+                  xAxes: [{
+                    barPercentage: 0.5,
+                  }]
+                  }
+             }
+
+        });
+    }, 100);
+  }
+
+  yearlyGraph(){
+    setTimeout(() => {
+        this.yearly = new Chart(this.yearlyCanvas.nativeElement, {     
+            type: 'bar',
+            data: {
+              labels: ["2015", "2016", "2017", "2018"],
+              datasets: [
+                  {
+                  lineTension: 0.1,
+                  backgroundColor: "rgba(22, 189, 231, 0.918)",
+                  data: [22, 32, 34, 16],
+                  }
+                ]
+              },
+              options: {
+                legend: {
+                  display: false
+                },
+                tooltips: {
+                  enabled: false
+                },
+                scales: {
+                  xAxes: [{
+                    barPercentage: 0.5,
+                  }]
+                  }
+             }
+
+        });
+    }, 100);
   }
 
   generationSummaryGraph() {
